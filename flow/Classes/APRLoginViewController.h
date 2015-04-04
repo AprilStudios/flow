24265cab41cf8b38bd9f0e3357b7d6318db63f58
Bohui Moon <moondennis@naver.com>
2015-04-04 00:41:22 -0700

    Added Facebook SDK & fixed build error
    - Fixed error where Xcode couldn't find info.plist

---
+++ flow/Classes/APRLoginViewController.h
@@ -0,0 +1,15 @@
+
+#import <UIKit/UIKit.h>
+
+/**
+ * @class APRLoginViewController
+ * @brief APRLoginViewController
+ *
+ * View Controller for Login screen.
+ *
+ * @author Bohui (Dennis) Moon
+ * @version 0.1
+ */
+@interface APRLoginViewController : UIViewController
+
+@end
