60501c225f602c1953cc017abaaff88d43123a9a
Bohui Moon <moondennis@naver.com>
2015-04-04 04:28:49 -0700

    Flow Circle now rotates until login

--- flow/Classes/APRLoginViewController.m
+++ flow/Classes/APRLoginViewController.m
@@ -4,8 +4,17 @@
 
 
 @interface APRLoginViewController ()
+{
+    BOOL stopSpin;
+}
 
+//IBOutlets
+@property (nonatomic, weak) IBOutlet UIImageView *flowCircle;
+@property (nonatomic, weak) IBOutlet UIButton *facebookButton;
 @property (nonatomic, weak) IBOutlet UIButton *toMainButton;
+
+//private helpers
+
 
 @end
 #pragma mark -
@@ -24,10 +33,23 @@
     [super viewDidLoad];
 
     //init any variables
+    stopSpin = NO;
     [self setNeedsStatusBarAppearanceUpdate];
-    //self.view.backgroundColor = [UIColor blackColor];//[UIColor colorWithPatternImage:[UIImage imageNamed:@"tweed_@2x"]];
 }
 
+
+/**
+ * @method viewWillLoad
+ *
+ * Called when this view is about to appear
+ * @note called every time this view is going to appear
+ */
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self rotateFlowCircle];
+}
 
 - (IBAction)toMain:(id)sender
 {
@@ -52,4 +74,37 @@
 }
 
 
+#pragma mark - Private Heleprs
+/**
+ * @method rotateFlowCircle
+ *
+ * Rotates the flow circle after a slight delay
+ * as long as stop animation flag is not false.
+ */
+- (void)rotateFlowCircle
+{
+    if (stopSpin)
+        return;
+    
+    [UIView animateWithDuration:1.0 delay:0.5
+    options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionCurveEaseInOut
+    animations:^
+    {
+        self.flowCircle.transform = CGAffineTransformRotate(self.flowCircle.transform, -M_PI_2);
+    }
+    completion:^(BOOL finished)
+    {
+        [self rotateFlowCircle];
+    }];
+}
+
 @end
+
+
+
+
+
+
+
+
+
