afd717e559893221b577041a76f6622179b94e57
Alex Hong <hongalex@usc.edu>
2015-04-04 17:16:23 -0700

    AppDelegate now ignored by this branch

--- flow/Classes/APRParseManager.swift
+++ flow/Classes/APRParseManager.swift
@@ -1,19 +1,25 @@
-//
-//  APRParseManager.swift
-//  flow
-//
-//  Created by Alex Hong on 4/4/15.
-//  Copyright (c) 2015 AprilStudios. All rights reserved.
-//
-
 import Foundation
+
+/**
+* @class APRParseManager
+* @brief APRParseManager
+*
+* This class handles direct interaction with Parse, including saving data in the form of PFObjects,
+* 
+*
+*
+*
+* @author Alex Hong, Kevin Wu
+* @version 0.1
+*/
+
 
 class APRParseManager: NSObject {
     override init() {
         super.init()
         println("SwiftClass init")
     }
-    
+    private var createdUserStatesID = false;
     func addUser(u: APRUser) {
         var tempUser = PFObject(className:"APRUser")
         tempUser["userID"] = u.getUserID()
@@ -41,35 +47,8 @@ class APRParseManager: NSObject {
             }
         }
     }
-    /*
-    Adds state s to a part of user u's states variable (An NSArray) and the amount of time they were in such a state
-    */
-    func addStateForUser(u: APRUser, s: APRState) {
-        var userQuery = PFQuery(className:"APRUser");
-        var stateQuery = PFQuery(className:"APRState")
-        userQuery.getObjectInBackgroundWithId(u.getObjectID()) {
-            (tempUser: PFObject!, error: NSError!) -> Void in
-            if error != nil {
-                println(error)
-            } else {
-                //User Found
-                stateQuery.getObjectInBackgroundWithId(s.getObjectID()) {
-                    (tempState: PFObject!, error: NSError!) -> Void in
-                    if error != nil {
-                        self.addCustomStateForUser(u, s: s)
-                    } else {
-                        //State found
-                        var stateIDs = [String]()
-                        stateIDs = (tempUser["stateIDs"] as? NSArray) as Array!
-                        stateIDs.append(s.getObjectID())
-                        tempUser["stateIDs"] = stateIDs
-                    }
-                }
-            }
-        }
-    }
-    
-    func addCustomStateForUser(u: APRUser, s: APRState) {
+
+    func addState(s: APRState, completion:((success:Bool)->Void)) {
         var tempState = PFObject(className:"APRState")
         tempState["stateID"] = s.getStateID()
         tempState["name"] = s.getName()
@@ -80,10 +59,61 @@ class APRParseManager: NSObject {
             if (success) {
                 var objID = tempState.objectId
                 s.setObjectID(objID)
-                self.addStateForUser(u, s: s)
             }
         }
+        completion(success: true)
     }
+    
+    /*
+    Adds state s to a part of user u's states variable (An NSArray) and the amount of time they were in such a state
+    */
+    /*func addStateForUser(u: APRUser, s: APRState) {
+        var userQuery = PFQuery(className:"APRUser");
+        var stateQuery = PFQuery(className:"APRState")
+        userQuery.getObjectInBackgroundWithId(u.getObjectID()) {
+            (tempUser: PFObject!, error: NSError!) -> Void in
+            if error != nil
+            {
+                println(error)
+            }
+            else
+            {
+                //User Found
+                stateQuery.getObjectInBackgroundWithId(s.getObjectID()) {
+                    (tempState: PFObject!, error: NSError!) -> Void in
+                    if error != nil
+                    {
+                        self.addState(s, completion: {(Bool) in
+                            //State found
+                            if (self.createdUserStatesID) {
+                                var userStatesID = [String]()
+                                userStatesID = (tempUser["userStatesID"] as? NSArray) as Array!
+                                userStatesID.append(s.getObjectID())
+                                tempUser["userStatesID"] = userStatesID
+                            } else {
+                                var userStatesID = ["2", "3"]
+                                tempUser["userStatesID"] = userStatesID
+                                self.createdUserStatesID = true
+                            }
+                        })
+                    }
+                    //State found
+                    if (self.createdUserStatesID) {
+                        var userStatesID = [String]()
+                        userStatesID = (tempUser["userStatesID"] as? NSArray) as Array!
+                        userStatesID.append(s.getObjectID())
+                        tempUser["userStatesID"] = userStatesID
+                    } else {
+                        var userStatesID = [s.getObjectID()]
+                        tempUser["userStatesID"] = userStatesID
+                        self.createdUserStatesID = true
+                    }
+                    tempState.saveInBackground()
+                    tempUser.saveInBackground()
+                }
+            }
+        }
+    }*/
     
     /*func makeUser(objectId:String)-> APRUser {
         var userQuery = PFQuery(className: "APRUser")
@@ -100,7 +130,7 @@ class APRParseManager: NSObject {
 
     }*/
 
-    func nicknameForUserID(u:NSString, completion:((nickname:NSString)->Void)) -> Void
+    func nicknameForUserID(u:NSString, completion:((nickname:NSString?)->Void)) -> Void
     {
         var userQuery = PFQuery(className: "APRUser")
         userQuery.whereKey("userID", equalTo:u)
@@ -112,10 +142,17 @@ class APRParseManager: NSObject {
                 println("Successfully retrieved")
                 if let objects = objects as? [PFObject]
                 {
-                    let n = objects[0]["nickname"] as NSString
-                    completion(nickname: n)
-                    
-                    return
+                    if objects.count==1
+                    {
+                        let n = objects[0]["nickname"] as NSString
+                        completion(nickname: n)
+                        return
+                    }
+                    else
+                    {
+                        completion(nickname: nil)
+                        return
+                    }
                 }
             }
             else
@@ -125,7 +162,7 @@ class APRParseManager: NSObject {
         }
     }
 
-    func userIDForNickname(nickname:NSString, completion:((userID:NSString)->Void)) -> Void
+    func userIDForNickname(nickname:NSString, completion:((userID:NSString?)->Void)) -> Void
     {
         var userQuery = PFQuery(className: "APRUser")
         userQuery.whereKey("nickname", equalTo:nickname)
@@ -137,10 +174,17 @@ class APRParseManager: NSObject {
                     println("Successfully retrieved")
                     if let objects = objects as? [PFObject]
                     {
-                        let n = objects[0]["userID"] as NSString
-                        completion(userID: n)
-                        
-                        return
+                        if objects.count==1
+                        {
+                            let n = objects[0]["userID"] as NSString
+                            completion(userID: n)
+                            return
+                        }
+                        else
+                        {
+                            completion(userID: nil)
+                            return
+                        }
                     }
                 }
                 else
--- flow/Classes/APRUser.swift
+++ flow/Classes/APRUser.swift
@@ -5,7 +5,12 @@ import Foundation
  * @class APRUser
  * @brief APRUser
  *
- * 
+ * The APRUser class stores key components about a specific user including:
+ * their name, facebookID, nickname. It also includes the user's current
+ * state, how long that state has been active, their custom states,
+ * the total time they have spent in every state, and the visibility of states
+ *
+ * Includes accessor/mutator functions to change above data members
  *
  * @author Alex Hong, Kevin Wu
  * @version 0.1
@@ -18,7 +23,7 @@ import Foundation
     private var objectID: String
     private var timeSinceChanged: NSDate
     private var stateTimes: Dictionary<APRState,Int32>
-    private var customStates:[APRState]
+    private var userStates:[APRState]
     private var stateVisabilities:Dictionary<APRState, Bool>
     
     override init()
@@ -29,7 +34,7 @@ import Foundation
         self.objectID=String()
         self.timeSinceChanged = NSDate()
         self.stateTimes = Dictionary<APRState,Int32>()
-        self.customStates = [APRState]()
+        self.userStates = [APRState]()
         self.stateVisabilities = Dictionary<APRState, Bool>()
         
         super.init()
@@ -43,11 +48,12 @@ import Foundation
         self.objectID=String()
         self.timeSinceChanged = NSDate()
         self.stateTimes = Dictionary<APRState,Int32>()
-        self.customStates = [APRState]()
+        self.userStates = [APRState]()
         self.stateVisabilities = Dictionary<APRState, Bool>()
         super.init()
     }
     
+    //Accessor functions
     func getState()-> APRState {
         return state
     }
@@ -66,12 +72,14 @@ import Foundation
     func getStateTimes()-> Dictionary<APRState,Int32> {
         return stateTimes
     }
-    func getCustomStates()->[APRState] {
-        return customStates
+    func getUserStates()->[APRState] {
+        return userStates
     }
     func getStateVisabilities()-> Dictionary<APRState, Bool> {
         return stateVisabilities
     }
+    
+    //Mutator functions
     func setState(s:APRState)-> Void {
         state = s
     }
@@ -81,9 +89,34 @@ import Foundation
     func setObjectID(o:String)-> Void{
         objectID = o
     }
+    /**
+    * @method <setTimeSinceChanged>
+    *
+    * @note Designed to be used alongside setState, when a state is about to change.
+    * Although written as mutator function with arguments,
+    * it typically will reset the timeSinceChanged to the current time
+    * which should be done in APRParseManager
+    *
+    *     *
+    * @param <NSDate> - date
+    *
+    * @return void
+    */
     func setTimeSinceChanged(date:NSDate)-> Void {
         timeSinceChanged = date
     }
+    
+    /**
+    * @method <addToStateTimes>
+    *
+    * @note Designed to be used alongside setState when a state is about to change.
+    * The state's time in action is recorded and added to the dictionary stateTimes
+    *
+    * @param <APRState> - userState
+    * @param <Int32> - 32-bit integer value
+    *
+    * @return void
+    */
     func addToStateTimes(s:APRState, i:Int32)-> Void {
         if(stateTimes[s]==nil) {
             stateTimes[s]=i
@@ -92,8 +125,14 @@ import Foundation
             stateTimes[s]=stateTimes[s]!+i
         }
     }
-    func addCustomState(s:APRState)-> Void {
-        customStates.append(s)
+    func addUserStates(s:APRState)-> Void {
+        for state in userStates {
+            if state.getObjectID() == s.getObjectID()
+            {
+                return
+            }
+        }
+        userStates.append(s)
     }
     func setStateVisabilities(s: APRState, b: Bool)-> Void {
         stateVisabilities[s]=b
